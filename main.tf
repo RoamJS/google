aef133589e765d73334f2b20a649c91283190cd1
David Vargas <dvargas92495@gmail.com>
2022-11-08 17:10:42 -0500

    google auth endpoint migrated to roamjs-backend

--- main.tf
+++ main.tf
@@ -3,13 +3,7 @@ terraform {
     hostname = "app.terraform.io"
     organization = "VargasArts"
     workspaces {
-      prefix = "roamjs-google"
-    }
-  }
-  required_providers {
-    github = {
-      source = "integrations/github"
-      version = "4.2.0"
+      name = "roamjs-google"
     }
   }
 }
@@ -49,34 +43,3 @@ provider "github" {
     token = var.github_token
 }
 
-module "roamjs_lambda" {
-  source = "dvargas92495/lambda/roamjs"
-  providers = {
-    aws = aws
-    github = github
-  }
-
-  name = "google"
-  lambdas = [
-    { 
-      path = "google-auth", 
-      method = "post"
-    },
-  ]
-  aws_access_token = var.aws_access_token
-  aws_secret_token = var.aws_secret_token
-  github_token     = var.github_token
-  developer_token  = var.developer_token
-}
-
-resource "github_actions_secret" "google_client_id" {
-  repository       = "roamjs-google"
-  secret_name      = "GOOGLE_CLIENT_ID"
-  plaintext_value  = var.google_client_id
-}
-
-resource "github_actions_secret" "google_client_secret" {
-  repository       = "roamjs-google"
-  secret_name      = "GOOGLE_CLIENT_SECRET"
-  plaintext_value  = var.google_client_secret
-}
